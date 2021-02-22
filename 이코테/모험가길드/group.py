8ef3ad38c27b953dcaabb02ff69451dbd9cd8ec3
ajongs <35219960+ajongs@users.noreply.github.com>
2021-02-22 13:36:01 +0900

    Create group.py

--- 이코테/모험가길드/group.py
+++ 이코테/모험가길드/group.py
@@ -1,1 +1,13 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
+count =0
+group =0
+for i in data:
+  count += 1
+  if count>=i:
+    group +=1
+    count =0
+
+print(group)
