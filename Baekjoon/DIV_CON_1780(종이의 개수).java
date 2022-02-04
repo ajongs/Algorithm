071540915bd44f64a175ff8e4e55dae8e29e3f08
park wonjong / 박원종 <wonjong1030@naver.com>
2022-02-04 23:33:42 +0900

    Resolve: Add DIV_CON_1780 (myself 2th)

--- Baekjoon/DIV_CON_1780(종이의 개수).java
+++ Baekjoon/DIV_CON_1780(종이의 개수).java
@@ -45,3 +45,48 @@ class Main{
         return true;
     }
 }
+
+/* review code
+import java.io.*;
+
+class Main{
+    static int[][] array;
+    static int[] cnt=new int[3]; //-1, 0 ,1 각각 1더한게 인덱스 자리
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        array = new int[n][n];
+        for(int i=0; i<n; i++){
+            String[] str = br.readLine().split(" ");
+            for(int j=0; j<n; j++){
+                array[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+        solve(0,0,n);
+        for(int c : cnt){
+            System.out.println(c);
+        }
+    }
+    public static void solve(int x, int y, int length){
+        if(isSame(x,y,length)){
+            cnt[array[x][y]+1] += 1;
+            return;
+        }
+        int div = length /3;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                solve(x+div*i, y+div*j, div);
+            }
+        }
+    }
+    public static boolean isSame(int x, int y, int length){
+        for(int i=x; i<x+length; i++){ // i<x+length 조심하자 0,0이 아닐때도 생각해야지
+            for(int j=y; j<y+length; j++){
+                if(array[x][y] != array[i][j])
+                    return false;
+            }
+        }
+        return true;
+    }
+}*/
