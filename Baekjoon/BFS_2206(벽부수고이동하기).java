c11c723193402a9323c10ec03271bce996a82a94
park wonjong / 박원종 <wonjong1030@naver.com>
2022-02-27 21:27:02 +0900

    Solve: Add BFS_2206 (reference)

--- Baekjoon/BFS_2206(벽부수고이동하기).java
+++ Baekjoon/BFS_2206(벽부수고이동하기).java
@@ -1,3 +1,77 @@
+
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static int[][] map;
+    static int[][][] dis;
+    static int n;
+    static int m;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        n = Integer.parseInt(str[0]);
+        m = Integer.parseInt(str[1]);
+        map = new int[n][m];
+        dis = new int[n][m][2];
+        for(int i=0; i<n; i++){
+            String[] temp = br.readLine().split("");
+            for(int j=0; j<m; j++){
+                map[i][j] = Integer.parseInt(temp[j]);
+            }
+        }
+        bfs();
+        if(dis[n-1][m-1][1] > 0  && dis[n-1][m-1][0] > 0){
+            System.out.println(Math.min(dis[n-1][m-1][1] , dis[n-1][m-1][0] ));
+        }
+        else if(dis[n-1][m-1][1] == 0 && dis[n-1][m-1][0] == 0){
+            System.out.println(-1);
+        }
+        else if(dis[n-1][m-1][1] == 0 || dis[n-1][m-1][0] == 0){
+            System.out.println(Math.max(dis[n-1][m-1][1] , dis[n-1][m-1][0]));
+        }
+    }
+    static void bfs(){
+        Queue<Point> q = new LinkedList<>();
+        dis[0][0][0] = 1;
+        int[] dx = {0,0,-1,1};
+        int[] dy = {-1,1,0,0};
+        q.offer(new Point(0,0,0));
+        while(!q.isEmpty()){
+            Point cp = q.poll();
+
+            for(int i=0; i<4; i++){
+                int x = cp.x + dx[i];
+                int y = cp.y + dy[i];
+
+                if(x>=0 && x<n && y>=0 && y<m){
+                    if(map[x][y] ==0 && dis[x][y][cp.z]==0){
+                        dis[x][y][cp.z] = dis[cp.x][cp.y][cp.z] + 1;
+                        q.offer(new Point(x,y, cp.z));
+
+                    }
+                    else if(cp.z==0 && map[x][y]==1 && dis[x][y][cp.z+1]==0  ){
+                        dis[x][y][cp.z+1] = dis[cp.x][cp.y][cp.z]+1;
+                        q.offer(new Point(x,y, 1));
+                    }
+                }
+
+            }
+        }
+    }
+    static class Point{
+        int x;
+        int y;
+        int z;
+        Point(int x, int y, int z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
+
+/*
 import java.io.*;
 import java.util.*;
 
@@ -75,3 +149,4 @@ class Main{
         }
     }
 }
+*/
