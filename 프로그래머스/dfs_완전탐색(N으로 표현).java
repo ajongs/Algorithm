fee08472c647b99bde26b545d0cc661170c3191e
park wonjong / 박원종 <wonjong1030@naver.com>
2022-03-14 23:58:58 +0900

    Half Solve: reference

---
+++ 프로그래머스/dfs_완전탐색(N으로 표현).java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    static int min = Integer.MAX_VALUE;
+    public int solution(int N, int number) {
+        dfs(0,N,number,0);
+        return min;
+    }
+    static void dfs(int depth, int N, int number, int current){
+        if(depth>8){
+            min = -1;
+            return;
+        }
+        if(current == number){
+            min = Math.min(min, depth);
+            return;
+        }
+        int temp=N;
+        for(int i=0; i<8-depth; i++){
+            int newDepth = depth + i+1;
+            dfs(newDepth, N, number, current+temp);
+            dfs(newDepth, N, number, current-temp);
+            dfs(newDepth, N, number, current*temp);
+            dfs(newDepth, N, number, current/temp);
+            temp = temp*10+N;
+
+        }
+    }
+}
