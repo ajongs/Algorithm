5a3a426954e0c60d2934ce4c7483bf2dbaab532b
park wonjong / 박원종 <wonjong1030@naver.com>
2022-03-17 23:26:43 +0900

    Not solve: reference

---
+++ 프로그래머스/java/Binary Search(입국심사).java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long left = 0;
+        long right = (long)times[times.length-1]*n; //최대로 걸리는 시간
+        while(left <= right){
+            long mid = (left + right) /2 ;
+            
+            long sum = 0;
+            for(int time : times){
+                sum += mid / time;
+            }
+            if(sum <n){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+                answer = mid;
+            }
+        }
+        return answer;
+    }
+}
