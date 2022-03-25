8947f53833f3d68f5931f7d9358063f00439f060
park wonjong / 박원종 <wonjong1030@naver.com>
2022-03-25 18:46:03 +0900

    Solve : myself 90%, reference 10%
    메모리초과를 계산하자.
    이전 코드들 같은 경우 배열을 사용해서 인접 행렬을 만들었다.
    문제에서 v(정점)의 개수가 20,000개 까지 가능하므로 인접행렬은 int 형이므로 4byte x 20,000 x 20,000 = 1600,000,000 byte
    -> 1600,000 KB, -> 1600MB 로 표현가능
    문제에서 메모리는 256 MB 이므로 초과함
    
    따라서, 인접리스트로 바꾸어 문제해결

--- Baekjoon/BFS_1701(이분그래프).java
+++ Baekjoon/BFS_1701(이분그래프).java
@@ -4,24 +4,29 @@ import java.util.*;
 class Main{
     static int v;
     static int e;
+    static List<Integer>[] list;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(br.readLine());
+
 
         for(int i=0; i<k; i++){
             String[] temp = br.readLine().split(" ");
             v = Integer.parseInt(temp[0]);
             e = Integer.parseInt(temp[1]);
-            int[][] adj = new int[v+1][v+1];
+            list = new ArrayList[v+1];
+            for(int z=1; z<v+1; z++){
+                list[z] = new ArrayList<>();
+            }
             for(int j=0; j<e; j++){
                 String[] vertex = br.readLine().split(" ");
-                adj[Integer.parseInt(vertex[0])][Integer.parseInt(vertex[1])] =1;
-                adj[Integer.parseInt(vertex[1])][Integer.parseInt(vertex[0])] =1;
+                list[Integer.parseInt(vertex[0])].add(Integer.parseInt(vertex[1]));
+                list[Integer.parseInt(vertex[1])].add(Integer.parseInt(vertex[0]));
             }
-            bfs(adj);
+            bfs();
         }
     }
-    public static void bfs(int[][] adj) {
+    public static void bfs() {
         Queue<Integer> q = new LinkedList<>();
         int[] type = new int[v+1];
         for(int i=1; i<v+1; i++){
@@ -31,20 +36,17 @@ class Main{
             }
             while(!q.isEmpty()){
                 int cur = q.poll();
-
-                for(int next=1; next<v+1; next++){
-                    if(adj[cur][next]==1){
-                        if(type[next]==0){
-                            if(type[cur]==1)
-                                type[next]=2;
-                            else
-                                type[next] =1;
-                            q.offer(next);
-                        }
-                        else if(type[next] == type[cur]){
-                            System.out.println("NO");
-                            return;
-                        }
+                for(int next : list[cur]){
+                    if(type[next]==0){
+                        if(type[cur]==1)
+                            type[next]=2;
+                        else
+                            type[next] =1;
+                        q.offer(next);
+                    }
+                    else if(type[next] == type[cur]){
+                        System.out.println("NO");
+                        return;
                     }
                 }
             }
