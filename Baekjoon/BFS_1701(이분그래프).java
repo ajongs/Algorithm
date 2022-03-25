ade45e9b98bf0ccea8f65ec19ae2531604c3d877
park wonjong / 박원종 <wonjong1030@naver.com>
2022-03-25 18:41:31 +0900

    Not solve: 메모리초과

--- Baekjoon/BFS_1701(이분그래프).java
+++ Baekjoon/BFS_1701(이분그래프).java
@@ -2,72 +2,53 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    static boolean visited[];
+    static int v;
+    static int e;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(br.readLine());
 
         for(int i=0; i<k; i++){
             String[] temp = br.readLine().split(" ");
-            int v = Integer.parseInt(temp[0]);
-            int e = Integer.parseInt(temp[1]);
+            v = Integer.parseInt(temp[0]);
+            e = Integer.parseInt(temp[1]);
             int[][] adj = new int[v+1][v+1];
             for(int j=0; j<e; j++){
                 String[] vertex = br.readLine().split(" ");
                 adj[Integer.parseInt(vertex[0])][Integer.parseInt(vertex[1])] =1;
                 adj[Integer.parseInt(vertex[1])][Integer.parseInt(vertex[0])] =1;
             }
-            checkBipartiteGraph(v,e,adj);
+            bfs(adj);
         }
     }
-    public static void checkBipartiteGraph(int v, int e, int[][] adj){
-        visited = new boolean[v+1];
-        boolean result = true;
+    public static void bfs(int[][] adj) {
+        Queue<Integer> q = new LinkedList<>();
+        int[] type = new int[v+1];
         for(int i=1; i<v+1; i++){
-            if(!visited[i]){
-                if(!bfs(v, i, adj))
-                    result = false;
+            if(type[i]==0){
+                type[i] = 1;
+                q.offer(i);
             }
-        }
-        if(result)
-            System.out.println("YES");
+            while(!q.isEmpty()){
+                int cur = q.poll();
 
-    }
-    public static boolean bfs(int v, int start, int[][] adj) {
-        boolean[] check = new boolean[v+1];
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(start, true));
-        visited[start] = true;
-        check[start] = true;
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-
-            for(int i=1; i<v+1; i++){
-                if(i==cur.vertex) continue;
-                if(adj[cur.vertex][i]==1){
-                    if(!visited[i]){
-                        visited[i] = true;
-                        check[i] = !cur.type;
-                        q.offer(new Node(i, !cur.type));
-                    }
-                    else{
-                        if(check[i] == cur.type){
+                for(int next=1; next<v+1; next++){
+                    if(adj[cur][next]==1){
+                        if(type[next]==0){
+                            if(type[cur]==1)
+                                type[next]=2;
+                            else
+                                type[next] =1;
+                            q.offer(next);
+                        }
+                        else if(type[next] == type[cur]){
                             System.out.println("NO");
-                            return false;
+                            return;
                         }
                     }
                 }
             }
         }
-        return true;
-    }
-    static class Node{
-        int vertex;
-        boolean type;
-        Node(int vertex, boolean type){
-            this.vertex = vertex;
-            this.type = type;
-        }
+        System.out.println("YES");
     }
 }
