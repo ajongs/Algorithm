2f8db2925b0512ded99738755750c85382a21ac6
park wonjong / 박원종 <wonjong1030@naver.com>
2022-11-10 17:40:11 +0900

    Solve: reference

---
+++ Baekjoon/DP_15989(1, 2, 3 더하기 4).java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_15989 {
+    static final int MAX = 10001;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[MAX][4];
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        for (int i = 4; i < MAX; i++) {
+            dp[i][1] = dp[i-1][1];
+            dp[i][2] = dp[i-2][1] + dp[i-2][2];
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
+        }
+    }
+}
